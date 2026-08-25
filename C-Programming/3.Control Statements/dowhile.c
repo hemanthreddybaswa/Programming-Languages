2c306085cce3f105bbfd450994eaa55c86f5b00e
Hemanth Reddy <hemanthreddybaswa@gmail.com>
2026-08-25 12:44:14 +0530

    Added some files

---
+++ C-Programming/3.Control Statements/dowhile.c
@@ -0,0 +1,28 @@
+//do while loop
+#include"stdio.h"
+#include"conio.h"
+int main()
+{
+	
+	int i=1;
+	do{
+		printf("%d ",i);
+		i++;
+	}while(i<=10);
+	
+	
+	/*
+	//Checking for valid mobile number using do while loop
+	long long int mobile;
+	do{
+		printf("Enter mobile number:");
+		scanf("%lld",&mobile);
+		
+	}while(!(mobile>=1000000000 && mobile<=9999999999));
+	
+	printf("Valid mobile number\n");
+	
+	*/
+	
+	getch();
+}
